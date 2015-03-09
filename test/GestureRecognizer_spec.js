04fdb3126d81f35da017409c437eacc376cd0cf8
marcomarchesi <marchesimarco@gmail.com>
2015-03-09 15:15:08 +0100

    init ann

--- test/GestureRecognizer_spec.js
+++ test/GestureRecognizer_spec.js
@@ -1,19 +1,52 @@
 var assert = require("assert");
 var gestureRecognizer = require("../lib/GestureRecognizer.js");
-var recognizer = new gestureRecognizer();
+// var recognizer = new gestureRecognizer();
 var test_data = [[23,45,6],[34,4,6],[4,35,2],[2,5,9],[3,25,89]];
+var brain = require('brain');
+var net = new brain.NeuralNetwork();
 
 describe('GestureRecognizer', function(){
   describe('load', function(){
     it('should return the data loaded', function(){
+      var recognizer = new gestureRecognizer();
       assert.equal(5, recognizer.load(test_data).length);
       // assert.equal(-1, [1,2,3].indexOf(0));
     });
   });
   describe('cluster',function(){
     it('should return the clusters', function(){
+      var recognizer = new gestureRecognizer();
       recognizer.data = test_data;
-      assert.equal([], recognizer.cluster(2));
+      assert.equal(2, recognizer.cluster(2).length);
+    });
+  });
+  /* valid gestures:
+  * 1. start mic
+  * 2. stop
+  * 3. walking 
+  * 4. circle
+  */
+  describe('set',function(){
+    it('should set a pair (glove_data,gesture)',function(){
+      var recognizer = new gestureRecognizer();
+      recognizer.set(test_data[0],recognizer.GESTURE_START_MIC);
+      assert.equal(1,recognizer.trainingSet.length);
+    });
+  });
+
+  describe('train',function(){
+    it('should train the training set made by pairs (glove_data,gesture)',function(){
+      var recognizer = new gestureRecognizer();
+      recognizer.set(test_data[0],recognizer.GESTURE_START_MIC);
+      recognizer.set(test_data[1],recognizer.GESTURE_STOP);
+      recognizer.set(test_data[2],recognizer.GESTURE_START_MIC);
+      recognizer.set(test_data[3],recognizer.GESTURE_CIRCLE);
+      recognizer.set(test_data[4],recognizer.GESTURE_WALKING);
+      // net.train(recognizer.trainingSet);
+      // var output = net.run([34,5,6]);
+      // console.log("output is " + output.stop);
+      console.log(recognizer.trainingSet);
+      recognizer.train(net,recognizer.trainingSet);
     });
   });
 
