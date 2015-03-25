33ea87a3d8b956a447002a0ddcc5b62e8b7ff7b6
marcomarchesi <marchesimarco@gmail.com>
2015-03-25 20:53:02 +0100

    yaw=0

--- glove_module/lib/glove.js
+++ glove_module/lib/glove.js
@@ -31,17 +31,20 @@ var GYRO_FACTOR = 14.375;
 var ACC_X_OFFSET = 0;
 var ACC_Y_OFFSET = 0;
 var ACC_Z_OFFSET = 38.46;
-var GYR_X_OFFSET = -2.00;
-var GYR_Y_OFFSET = -0.9;
-var GYR_Z_OFFSET = 0.13;
+var GYR_X_OFFSET = -1.7;
+var GYR_Y_OFFSET = 0;
+var GYR_Z_OFFSET = 0.00;
 var COM_X_OFFSET = 38;
 var COM_Y_OFFSET = 27.5;
 var COM_Z_OFFSET = -25;
 var COM_X_SCALE = 0.97;
 var COM_Y_SCALE = 0.97;
 var COM_Z_SCALE = 1.05;
-var ALPHA = 0.99;
-var BETA = 0.01;
+var ALPHA = 0.9;
+var BETA = 0.1;
+var com_x_offset = 0;
+var com_y_offset = 0;
+var com_z_offset = 0;
 
 var SAMPLE_DIM = 6;
 var DECIMAL_PRECISION = 4;
@@ -61,7 +64,7 @@ var io = require('socket.io').listen(8001);
 console.log("Hello Glove!");
 
 var quaternion = require("./Quaternion.js");
-var q = new quaternion(1,10);
+var q = new quaternion(0.4,10);
 
 var Recognizer = require("./GestureRecognizer.js");
 var recognizer = new Recognizer();
@@ -76,42 +79,46 @@ var sp = new serialport("/dev/cu.AmpedUp-AMP-SPP", {
   rtscts: false,
   flowControl: false
 });
-//open serial port
-sp.on("open", function () {
-    console.log('Serial port is open');
-    sp.write(READ_CMD);
-});
 
-sp.on('data',function(data){
-  // console.log(data);
-  // sp.write(READ_CMD);
+    /* OPEN SERIAL PORT */
+    /********************/
+    sp.on("open", function () {
+        console.log('Serial port is open');
+        sp.write(READ_CMD);
+    });
+
+    sp.on('data',function(data){
+      // console.log(data);
+      // sp.write(READ_CMD);
+      
+          for(var i = 0;i<data.length;++i){
+            buffer[byteCounter] = data[i]; 
+            byteCounter++;
+            // console.log(j);
+          }
+          if(byteCounter==21)
+            sendData();
+
+    });
+    sp.on('error',function(error){
+      console.log(chalk.red(error));
+    });
   
-      for(var i = 0;i<data.length;++i){
-        buffer[byteCounter] = data[i]; 
-        byteCounter++;
-        // console.log(j);
-      }
-      if(byteCounter==21)
-        sendData();
+    /* uncomment for LEAP MOTION */
+    // controller.loop(function(frame) {
 
-});
-sp.on('error',function(error){
-  console.log(chalk.red(error));
-});
-
-
-    controller.loop(function(frame) {
-
-            for (var i in frame.handsMap) {
-              leapHand = frame.handsMap[i];
-              // console.log(leapHand.roll());
-              io.sockets.emit('data',{roll:leapHand.roll(),pitch:leapHand.pitch(),yaw:leapHand.yaw()});
-            }
+    //         for (var i in frame.handsMap) {
+    //           leapHand = frame.handsMap[i];
+    //           // console.log(leapHand.roll());
+    //           io.sockets.emit('data',{roll:leapHand.roll(),pitch:leapHand.pitch(),yaw:leapHand.yaw()});
+    //         }
             
-    });
-    controller.on('ready', function() {
-          console.log(chalk.green("Leap Motion ready"));
-    });
+    // });
+    // controller.on('ready', function() {
+    //       console.log(chalk.green("Leap Motion ready"));
+    // });
+
+    /* *********************** */
 
 
 function sendData(){
@@ -122,12 +129,23 @@ function sendData(){
       acc_x = (buffer.readInt16LE(4) + ACC_X_OFFSET)*G_FACTOR;
       acc_y = -(buffer.readInt16LE(2) + ACC_Y_OFFSET)*G_FACTOR;
       acc_z = (buffer.readInt16LE(6) + ACC_Z_OFFSET)*G_FACTOR;
+
       gyr_x = -(buffer.readInt16LE(10)/GYRO_FACTOR) + GYR_X_OFFSET;
       gyr_y = (buffer.readInt16LE(8)/GYRO_FACTOR) + GYR_Y_OFFSET;
       gyr_z = (buffer.readInt16LE(12)/GYRO_FACTOR) + GYR_Z_OFFSET;
-      com_x = COM_X_SCALE * (buffer.readInt16LE(16) - COM_Y_OFFSET);
-      com_y = COM_Y_SCALE * (buffer.readInt16LE(14) - COM_X_OFFSET);
-      com_z = COM_Z_SCALE * (buffer.readInt16LE(18) - COM_Z_OFFSET);
+
+      com_x = COM_X_SCALE * (buffer.readInt16LE(16) - COM_Y_OFFSET) - com_x_offset;
+      com_y = COM_Y_SCALE * (buffer.readInt16LE(14) - COM_X_OFFSET) - com_y_offset;
+      com_z = COM_Z_SCALE * (buffer.readInt16LE(18) - COM_Z_OFFSET) - com_z_offset;
+
+      if(com_x_offset == 0)
+        com_x_offset = com_x;
+
+      if(com_y_offset == 0)
+        com_y_offset = com_y;
+
+      if(com_z_offset == 0)
+        com_z_offset = com_z;
 
       // acc_x = (buffer.readInt16LE(2) + ACC_X_OFFSET)*G_FACTOR;
       // acc_y = (buffer.readInt16LE(4) + ACC_Y_OFFSET)*G_FACTOR;
@@ -139,6 +157,8 @@ function sendData(){
       // com_y = COM_Y_SCALE * (buffer.readInt16LE(16) - COM_Y_OFFSET);
       // com_z = COM_Z_SCALE * (buffer.readInt16LE(18) - COM_Z_OFFSET);
 
+
+      /* VALUES FOR COMPASS CALIBRATION */
       // com_x_max = Math.max(com_x_max,com_x);
       // com_x_min = Math.min(com_x_min,com_x);
       // com_y_max = Math.max(com_y_max,com_y);
@@ -147,19 +167,20 @@ function sendData(){
       // com_z_min = Math.min(com_z_min,com_z);
 
       // console.log(chalk.yellow("acc_x " + acc_x + " acc_y " + acc_y + " acc_z " + acc_z));
-      console.log(chalk.yellow("gyr_x " + gyr_x + " gyr_y " + gyr_y + " gyr_z " + gyr_z));
-
+      // console.log(chalk.yellow("gyr_x " + gyr_x + " gyr_y " + gyr_y + " gyr_z " + gyr_z));
+      console.log(chalk.yellow("com_x " + com_x + " com_y " + com_y + " com_z " + com_z));
       q.update(acc_x,acc_y,acc_z,degreesToRadians(gyr_x),degreesToRadians(gyr_y),degreesToRadians(gyr_z));
       q.computeEuler();
 
       // var roll = q.getRoll();
       // var pitch = q.getPitch();
-      var yaw = ALPHA * degreesToRadians(gyr_z) ;
-      // + BETA * degreesToRadians(com_z);
-      // var yaw = 0;
+      // var yaw = ALPHA * degreesToRadians(gyr_z) + BETA * degreesToRadians(com_y);
+      // var yaw = degreesToRadians(ALPHA*com_x + BETA*gyr_z);
+      var yaw = 0;
+      // var yaw = Math.PI/2 + degreesToRadians(com_y);
       // var yaw = q.getYaw();
-      var pitch = Math.atan(acc_y/Math.sqrt(acc_x*acc_x + acc_z*acc_z));
-      var roll = Math.atan(-acc_x/acc_z);
+      var roll = Math.atan(acc_y/Math.sqrt(acc_x*acc_x + acc_z*acc_z));
+      var pitch = Math.atan(acc_x/acc_z);
       
       // console.log(yaw);
 
